7beb4b56a570041b3b6dc0822430d4b2a518540a
Michael Podoprigora <52321995+MikePodoprigora@users.noreply.github.com>
2021-01-26 22:59:31 -0500

    test is finished

---
+++ src/test/java/com/ilcarro/qa/CreateAccountTests.java
@@ -0,0 +1,5 @@
+package com.ilcarro.qa;
+
+public class CreateAccountTests extends TestBase{
+
+}
---
+++ src/test/java/com/ilcarro/qa/OpenHomePageTests.java
@@ -0,0 +1,69 @@
+package com.ilcarro.qa;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
+public class OpenHomePageTests extends TestBase{
+
+    WebDriver wd;
+
+    @BeforeMethod
+    public void setUp(){
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wd.manage().window().maximize();
+
+        wd.get("https://ilcarro-dev-v1.firebaseapp.com/");
+
+    }
+
+    @Test
+    public void homePageTest(){
+        System.out.println("Site is opened");
+
+        //wd.findElement(By.cssSelector(".Main_mainpage__find_your_car__AHLkw form"));
+        System.out.println("FindCarForm : " + isFindCarFormPresent());
+//        isFindCarFormPresent2();
+    }
+
+    //1t method
+    public boolean isElementPresent(By locator){
+        return wd.findElements(locator).size()>0;
+    }
+    public boolean isFindCarFormPresent(){
+        return isElementPresent(By.cssSelector(".Main_mainpage__find_your_car__AHLkw form"));
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    // 2nd method
+//    public boolean isFindCarFormPresent2(){
+//        return isElementPresent2(By.cssSelector(".Main_mainpage__find_your_car__AHLkw form");
+//    }
+//
+//
+    public boolean isElementPresent2(By by){
+        try{
+            wd.findElement(by);
+            return true;
+        }catch (NoSuchElementException ex){
+            return false;
+        }
+    }
+
+
+
+    @AfterMethod(enabled = false)
+    public void tearDown(){
+        wd.quit();
+
+    }
+}
