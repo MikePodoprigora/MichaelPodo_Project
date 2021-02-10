fa5cfb635eb6e4aef572590d3d632aaca3f43973
Michael Podoprigora <52321995+MikePodoprigora@users.noreply.github.com>
2021-02-09 22:43:35 -0500

    next commit

--- src/test/java/com/ilcarro/qa/TestBase.java
+++ src/test/java/com/ilcarro/qa/TestBase.java
@@ -5,11 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
