4d1d681fd959a8e3b150e8e2f0b6831b0c482349
Michael Podoprigora <52321995+MikePodoprigora@users.noreply.github.com>
2021-02-22 00:06:06 -0500

    Created Car Object/ Added fillCarForm

--- src/test/java/com/ilcarro/qa/AddCarTests.java
+++ src/test/java/com/ilcarro/qa/AddCarTests.java
@@ -6,30 +6,57 @@ import org.testng.annotations.Test;
 public class AddCarTests extends TestBase {
     @Test
     public void testFormLetTheCarWork(){
-//        click(By.cssSelector(".let-carwork-style_let_car_location__30BIh"));
+        click(By.cssSelector("[href='/car']"));
 
 
-        type(By.name("country"), "Canada");
-        type(By.cssSelector(".address"), "Bloor Street");
-        type(By.cssSelector(".distance_included"), "500");
-        type(By.cssSelector(".serial_number"), "12345678");
-        type(By.cssSelector(".brand"), "Volkswagen");
-        type(By.cssSelector(".model"), "Atlas");
-        type(By.cssSelector(".year"), "2021");
-        type(By.cssSelector("engine"), "3.5");
-        type(By.cssSelector(".transmition"), "Automatic");
-        type(By.cssSelector(".wd"), "gregfregfre");
-        type(By.cssSelector(".horsepower"), "180");
-        type(By.cssSelector(".torque"), "3456");
-        type(By.cssSelector(".doors"), "4");
-        type(By.cssSelector(".seats"), "5");
-        type(By.cssSelector(".class"), "E");
-        type(By.name("about"), "ewfeybrfyegyfgyerg");
-        type(By.cssSelector(".type_feature"), "egregegfreg");
-        type(By.cssSelector(".price"), "40");
+        fillCarForm(new Car()
+                .setCountry("Canada")
+                .setAddress("Bloor Street")
+                .setDistance_included("500")
+                .setSerial_number("12345678")
+                .setBrand("Volkswagen")
+                .setModel("Atlas")
+                .setYear("2021")
+                .setEngine("3.5V6")
+                .setFuel_consumption("11")
+                .setFuel("92")
+                .setTransmission("Automatic")
+                .setWd("gregfregfre")
+                .setHorsepower("180")
+                .setTorque("3456")
+                .setDoors("5")
+                .setSeats("5")
+                .setAuto_class("E")
+                .setAbout("ewfeybrfyegyfgyerg")
+                .setType_feature("egregegfreg")
+                .setPrice("40"));
+
 
         submitForm();
 
+    }
+
+    public void fillCarForm(Car car) {
+        type(By.name("country"), car.getCountry());
+        type(By.cssSelector(".address"), car.getAddress());
+        type(By.cssSelector(".distance_included"), car.getDistance_included());
+        type(By.cssSelector(".serial_number"), car.getSerial_number());
+        type(By.cssSelector(".brand"), car.getBrand());
+        type(By.cssSelector(".model"), car.getModel());
+        type(By.cssSelector(".year"), car.getYear());
+        type(By.name("engine"), car.getEngine());
+        type(By.name("fuel_consumption"), car.getFuel_consumption());
+        type(By.name("fuel"), car.getFuel());
+        type(By.cssSelector(".transmition"), car.getTransmission());
+        type(By.cssSelector(".wd"), car.getWd());
+        type(By.cssSelector(".horsepower"), car.getHorsepower());
+        type(By.cssSelector(".torque"), car.getTorque());
+        type(By.cssSelector(".doors"), car.getDoors());
+        type(By.cssSelector(".seats"), car.getSeats());
+        type(By.cssSelector(".class"), car.getAuto_class());
+        type(By.name("about"), car.getAbout());
+        type(By.cssSelector(".type_feature"), car.getType_feature());
+        type(By.cssSelector(".price"), car.getPrice());
 
     }
 
---
+++ src/test/java/com/ilcarro/qa/Car.java
@@ -0,0 +1,209 @@
+package com.ilcarro.qa;
+
+public class Car {
+    private  String country;
+    private  String address;
+    private  String distance_included;
+    private  String serial_number;
+    private  String brand;
+    private  String model;
+    private  String year;
+    private  String engine;
+    private  String fuel_consumption;
+    private  String fuel;
+    private  String transmission;
+    private  String wd;
+    private  String horsepower;
+    private  String torque;
+    private  String doors;
+    private  String seats;
+    private  String auto_class;
+    private  String about;
+    private  String type_feature;
+    private  String price;
+
+
+    public Car setCountry(String country) {
+        this.country = country;
+        return this;
+    }
+
+    public Car setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Car setDistance_included(String distance_included) {
+        this.distance_included = distance_included;
+        return this;
+    }
+
+    public Car setSerial_number(String serial_number) {
+        this.serial_number = serial_number;
+        return this;
+    }
+
+    public Car setBrand(String brand) {
+        this.brand = brand;
+        return this;
+    }
+
+    public Car setModel(String model) {
+        this.model = model;
+        return this;
+    }
+
+    public Car setYear(String year) {
+        this.year = year;
+        return this;
+    }
+
+    public Car setEngine(String engine) {
+        this.engine = engine;
+        return this;
+    }
+
+    public Car setFuel_consumption(String fuel_consumption) {
+        this.fuel_consumption = fuel_consumption;
+        return this;
+    }
+
+    public Car setFuel(String fuel) {
+        this.fuel = fuel;
+        return this;
+    }
+
+    public Car setTransmission(String transmission) {
+        this.transmission = transmission;
+        return this;
+    }
+
+    public Car setWd(String wd) {
+        this.wd = wd;
+        return this;
+    }
+
+    public Car setHorsepower(String horsepower) {
+        this.horsepower = horsepower;
+        return this;
+    }
+
+    public Car setTorque(String torque) {
+        this.torque = torque;
+        return this;
+    }
+
+    public Car setDoors(String doors) {
+        this.doors = doors;
+        return this;
+    }
+
+    public Car setSeats(String seats) {
+        this.seats = seats;
+        return this;
+    }
+
+    public Car setAuto_class(String auto_class) {
+        this.auto_class = auto_class;
+        return this;
+    }
+
+    public Car setAbout(String about) {
+        this.about = about;
+        return this;
+    }
+
+    public Car setType_feature(String type_feature) {
+        this.type_feature = type_feature;
+        return this;
+    }
+
+    public Car setPrice(String price) {
+        this.price = price;
+        return this;
+    }
+
+
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDistance_included() {
+        return distance_included;
+    }
+
+    public String getSerial_number() {
+        return serial_number;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public String getFuel_consumption() {
+        return fuel_consumption;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public String getWd() {
+        return wd;
+    }
+
+    public String getHorsepower() {
+        return horsepower;
+    }
+
+    public String getTorque() {
+        return torque;
+    }
+
+    public String getDoors() {
+        return doors;
+    }
+
+    public String getSeats() {
+        return seats;
+    }
+
+    public String getAuto_class() {
+        return auto_class;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public String getType_feature() {
+        return type_feature;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+
+}
